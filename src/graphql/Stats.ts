807e3eaeb9300ef43e08757be8cb15b33be36bd0
AlphaCodder <srjoethef@gmail.com>
2022-11-25 15:24:31 +0530

    obselete removal

--- src/graphql/Stats.ts
+++
@@ -1,9 +0,0 @@
-// make a pothos simple object names stats with a type of Stats
-import SchemaBuilder from '@pothos/core';
-import SimpleObjectsPlugin from '@pothos/plugin-simple-objects';
-
-
-const builder = new SchemaBuilder({
-    plugins: [SimpleObjectsPlugin],
-});
-
